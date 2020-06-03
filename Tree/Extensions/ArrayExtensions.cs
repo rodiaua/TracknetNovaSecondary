61c4661e9c803da3cb5fd5f0fae3728358179cc7
rodiaua <rodiaua@gmail.com>
2020-06-03 18:37:48 +0300

    ArrayExtensions moved to Extensions folder

---
+++ Tree/Extensions/ArrayExtensions.cs
@@ -0,0 +1,63 @@
+﻿namespace Tree.Extensions
+
+{
+  static class ArrayExtensions
+  {
+    /// <summary>
+    ///The method compare two arays and return true if element of the second array equals the elements of first array in the same order
+    ///and the length of the first  array is >= then length of second array.
+    ///So we can detect appropriate tree node and its children
+    /// </summary>
+    public static bool Compare(this int[] child, int[] parent)
+    {
+      if (child.Length >= parent.Length)
+      {
+        int n = 0;
+        for (int i = 0; i < parent.Length; i++)
+        {
+          if (child[i] == parent[i]) n++;
+        }
+        if (n == parent.Length) return true;
+      }
+      return false;
+    }
+
+    /// <summary>
+    ///The method compare two arays and return true if element of the second array equals the elements of first array in the same order
+    ///and the length of the first array is > then length of second array.
+    ///So we can detect children those are relevant to the tree node
+    /// </summary>
+    public static bool IsChildOf(this int[] child, int[] parent)
+    {
+      if (child.Length > parent.Length)
+      {
+        int n = 0;
+        for (int i = 0; i < parent.Length; i++)
+        {
+          if (child[i] == parent[i]) n++;
+        }
+        if (n == parent.Length) return true;
+      }
+      return false;
+    }
+
+    /// <summary>
+    ///The method compare two arays and return true if element of the second array equals the elements of first array in the same order
+    ///and the length-1 of the first array is = to length of second array.
+    ///So we can detect first cicle children those are relevant to the tree node
+    /// </summary>
+    public static bool IsFirstCicleChildOf(this int[] child, int[] parent)
+    {
+      if (child.Length - 1 == parent.Length)
+      {
+        int n = 0;
+        for (int i = 0; i < parent.Length; i++)
+        {
+          if (child[i] == parent[i]) n++;
+        }
+        if (n == parent.Length) return true;
+      }
+      return false;
+    }
+  }
+}
--- Tree/Services/MeterLocationTreeService.cs
+++ Tree/Services/MeterLocationTreeService.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Tree.Data;
+using Tree.Extensions;
 using Tree.Models;
 using Tree.Services;
 
@@ -155,66 +156,6 @@ namespace Tree.Services
       var elementThatContains = _dbContext.MeterLocationTree.Where(p => p.LocationElement.ToLower().Contains(substring.ToLower()) && !p.LocationElement.ToLower().StartsWith(substring.ToLower())).ToList();
       elementThatStarts.AddRange(elementThatContains);
       return elementThatStarts;
-    }
-  }
-
-  static class Extensions
-  {
-    /// <summary>
-    ///The method compare two arays and return true if element of the second array equals the elements of first array in the same order
-    ///and the length of the first  array is >= then length of second array.
-    ///So we can detect appropriate tree node and its children
-    /// </summary>
-    public static bool Compare(this int[] child, int[] parent)
-    {
-      if (child.Length >= parent.Length)
-      {
-        int n = 0;
-        for (int i = 0; i < parent.Length; i++)
-        {
-          if (child[i] == parent[i]) n++;
-        }
-        if (n == parent.Length) return true;
-      }
-      return false;
-    }
-
-    /// <summary>
-    ///The method compare two arays and return true if element of the second array equals the elements of first array in the same order
-    ///and the length of the first array is > then length of second array.
-    ///So we can detect children those are relevant to the tree node
-    /// </summary>
-    public static bool IsChildOf(this int[] child, int[] parent)
-    {
-      if (child.Length > parent.Length)
-      {
-        int n = 0;
-        for (int i = 0; i < parent.Length; i++)
-        {
-          if (child[i] == parent[i]) n++;
-        }
-        if (n == parent.Length) return true;
-      }
-      return false;
-    }
-
-    /// <summary>
-    ///The method compare two arays and return true if element of the second array equals the elements of first array in the same order
-    ///and the length-1 of the first array is = to length of second array.
-    ///So we can detect first cicle children those are relevant to the tree node
-    /// </summary>
-    public static bool IsFirstCicleChildOf(this int[] child, int[] parent)
-    {
-      if (child.Length - 1 == parent.Length)
-      {
-        int n = 0;
-        for (int i = 0; i < parent.Length; i++)
-        {
-          if (child[i] == parent[i]) n++;
-        }
-        if (n == parent.Length) return true;
-      }
-      return false;
     }
   }
 }
